b799bde9280855fea5bcca2b12ecc7256081684a
Abdelmoughit Moubarik <86983431+TIRGANI@users.noreply.github.com>
2022-04-29 14:39:01 +0000

    ajoutit khalid

--- chatbot.py
+++ chatbot.py
@@ -1,7 +1,7 @@
 import re
 import long_responses as long
 
-
+#khaliiiiiiiiiiiiiiiiiiiiiiiiiiiiiid
 def message_probability(user_message, recognised_words, single_response=False, required_words=[]):
     message_certainty = 0
     has_required_words = True
